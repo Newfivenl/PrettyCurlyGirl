468f5cb8f7718745ba4ea942f89a094e3ce15a2f
Newfive <info@newfive.nl>
2023-08-18 14:43:19 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -93,6 +93,7 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
-    require("@tailwindcss/forms")
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/nesting")
   ],
 };
